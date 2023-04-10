0f1f0db00ecaa0b6833ff11d9cbf49a6501f650c
Anurag <anurag@git79.com>
2023-04-10 20:52:48 +0530

    Liquid Swipe

--- app/src/main/java/com/example/akportfolio/MainActivity.kt
+++ app/src/main/java/com/example/akportfolio/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.akportfolio
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cuberto.liquid_swipe.LiquidPager
+import com.example.akportfolio.liquidswipe.Adapter
+
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pager = findViewById<LiquidPager>(R.id.pager)
+        pager.adapter = Adapter(supportFragmentManager)
     }
 }
--- app/src/main/java/com/example/akportfolio/SplashScreen.kt
+++ app/src/main/java/com/example/akportfolio/SplashScreen.kt
@@ -2,18 +2,24 @@ package com.example.akportfolio
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.media.Image
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+//import com.example.akportfolio.liquidswipe.PageFragment
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
 //    Fade Animation
     private lateinit var Image:ImageView
     private lateinit var TextView:TextView
+    private lateinit var Image1:ImageView
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,17 +28,21 @@ class SplashScreen : AppCompatActivity() {
 
 //      Splash Screen Code Animation
         Image = findViewById(R.id.ImageView)
-
+        Image1 = findViewById(R.id.img)
         TextView = findViewById(R.id.textView2)
         Image.alpha = 0f
+
+
         Image.animate().setDuration(2000).alpha(1f).withEndAction {
 
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+
+
             finish()
         }
 
-
     }
+
 }
